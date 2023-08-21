3db1251739f6c5d59dddfe5228fed63a269ba1a1
andy2 <andy.shaw@screenmedia.co.uk>
2023-08-21 15:51:04 +0100

    add

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" class="wrapper">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import '@/scss/styles.scss';
+</style>
